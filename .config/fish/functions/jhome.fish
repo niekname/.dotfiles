41818ba8a1b44e829ea930bb213b28a5d1b56eee
Niek Vervaeke <niek.vervaeke@gmail.com>
2019-05-25 00:02:30 +0200

    extended jhome function to list java versions

--- .config/fish/functions/jhome.fish
+++ .config/fish/functions/jhome.fish
@@ -1,3 +1,7 @@
 function jhome
-    set -U JAVA_HOME (/usr/libexec/java_home --version $argv) 
+    if set -q argv[1]
+        set -U JAVA_HOME (/usr/libexec/java_home --version $argv) 
+    else
+        /usr/libexec/java_home -V
+    end
 end
