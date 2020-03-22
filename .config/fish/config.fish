5ee233c1e5bf4ed594d43db8c08dd12d00113290
Niek Vervaeke <niek.vervaeke@gmail.com>
2020-03-22 16:04:01 +0100

    added fisher

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,9 @@
 set -x JAVA_HOME (/usr/libexec/java_home --version 1.8)
 set -x AWS_PROFILE masl
 alias mongod="mongod --config /usr/local/etc/mongod.conf"
+
+if not functions -q fisher
+    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+    fish -c fisher
+end
