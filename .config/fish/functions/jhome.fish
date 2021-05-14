c602b61230a16796f760d5729539deaac1ee48c7
Niek Vervaeke <niek.vervaeke@gmail.com>
2021-05-14 11:47:57 +0200

    switch to zsh / omz + some other changes

--- .config/fish/functions/jhome.fish
+++
@@ -1,7 +0,0 @@
-function jhome
-    if set -q argv[1]
-        set -U JAVA_HOME (/usr/libexec/java_home --version $argv) 
-    else
-        /usr/libexec/java_home -V
-    end
-end
---
+++ .zshrc
@@ -0,0 +1,8 @@
+. ~/.zfunctions
+
+export ZSH="/Users/niek/.oh-my-zsh"
+ZSH_THEME="robbyrussell"
+
+plugins=(git)
+
+source $ZSH/oh-my-zsh.sh
